10f6c40979bccbc199abd78ae863f120550cbdd3
Regis Tesson <registesson@gmail.com>
2021-02-06 15:21:11 +0100

    Use PatternFly

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import { Card, CardTitle, CardBody, Button } from '@patternfly/react-core'
+
+// HACK: implicit imports don't work: https://github.com/snowpackjs/snowpack/discussions/2120
+import '@patternfly/patternfly/patternfly.css'
+
+import './index.css'
 
 const Application = () => {
   const [counter, setCounter] = useState(0)
   return (
     <>
-      <h1>Snowpack Learn playground</h1>
-
-      <p>Counter is {counter}</p>
-      <button onClick={() => setCounter(counter + 1)}>Increase</button>
+      <Card>
+        <CardTitle>Counter</CardTitle>
+        <CardBody>
+          <p>{counter}</p>
+          <Button onClick={() => setCounter(counter + 1)}>
+            Increase Counter
+          </Button>
+        </CardBody>
+      </Card>
     </>
   )
 }
