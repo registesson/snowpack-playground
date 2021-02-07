77fc4d9df6dd99ebacc03ae861d07f2abf753252
Regis Tesson <registesson@gmail.com>
2021-02-07 15:08:00 +0100

    Integrate eslint into snowpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,7 +7,13 @@ module.exports = {
     src: '/',
   },
   plugins: [
-    /* ... */
+    [
+      '@snowpack/plugin-run-script',
+      {
+        cmd: 'eslint --ext .js,.jsx src/',
+        watch: 'esw -w --clear --ext .js,.jsx src/',
+      },
+    ],
   ],
   packageOptions: {
     /* ... */
